658009791b387fa997efb6e21b9d411956813161
ShahzaadAhmedSabeeh <ss0303@srmist.edu.in>
2025-08-14 11:05:55 +0530

    3rd commit

--- StringLengthFinder.java
+++ StringLengthFinder.java
@@ -12,7 +12,7 @@ public class StringLengthFinder {
         } catch (StringIndexOutOfBoundsException e) {
         }
         return count;
-    }
+    } //comment
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
